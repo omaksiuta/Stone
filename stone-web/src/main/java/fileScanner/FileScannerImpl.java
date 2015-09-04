b8496ce8995f07a9a4777ac3a51fd30ec7c49c2b
Igor Levkov <levkov.igor@gmail.com>
2015-09-04 14:42:20 +0300

    Sheduling via annotation

--- stone-web/src/main/java/fileScanner/FileScannerImpl.java
+++ stone-web/src/main/java/fileScanner/FileScannerImpl.java
@@ -1,18 +1,23 @@
 package fileScanner;
 
 import java.util.Date;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by igor on 04.09.2015.
  */
+@Service
 public class FileScannerImpl implements FileScanner {
 
     @Override
+    @Scheduled(cron = "0 0 12 * * ?")
     public void deleteOldVideo() {
         System.out.println(new Date() + ": old Videos DELETED");
     }
 
     @Override
+    @Scheduled(cron = "0 0 12 * * ?")
     public void deleteOldPhoto() {
         System.out.println(new Date() + ": old Photos DELETED");
     }
